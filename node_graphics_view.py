ae4aa31701f956688318b210719b00887436ab09
BadbadLoli <617361995@qq.com>
2022-01-26 23:34:12 +0800

    learning-02: 自定义view类来替换wnd中的view

---
+++ node_graphics_view.py
@@ -0,0 +1,19 @@
+from PySide2.QtWidgets import QGraphicsView
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+
+class QDMGraphicsView(QGraphicsView):
+    def __init__(self, grScene, parent=None):
+        super().__init__(parent)
+
+        self.grScene = grScene
+        self.initUI()
+        self.setScene(self.grScene)
+
+    def initUI(self):
+        self.setRenderHints(QPainter.Antialiasing | QPainter.HighQualityAntialiasing |
+                            QPainter.TextAntialiasing | QPainter.SmoothPixmapTransform)
+
+        self.setViewportUpdateMode(QGraphicsView.FullViewportUpdate)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
