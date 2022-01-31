7a681050fcc98fecf958892554411f0714f5e060
luoruchong <luoruchong@bytedance.com>
2022-01-31 22:06:39 +0800

    learning-09: 让edge宇socket连接起来，让edge有个起止点

--- node_socket.py
+++ node_socket.py
@@ -5,6 +5,7 @@ LEFT_BOTTOM = 2
 RIGHT_TOP = 3
 RIGHT_BOTTOM = 4
 
+DEBUG = False
 
 class Socket():
     def __init__(self, node, index=0, position=LEFT_TOP):
@@ -13,6 +14,19 @@ class Socket():
         self.index = index
         self.position = position
 
+        if DEBUG: print("Socket -- creating with", self.index, self.position, "for node", self.node)
+
         self.grSocket = QDMGraphicsSocket(self.node.grNode)
 
         self.grSocket.setPos(*self.node.getSocketPosition(index, position))
+
+        self.edge = None
+
+    def getSocketPosition(self):
+        if DEBUG: print("  GSP: ", self.index, self.position, "node:", self.node)
+        res = self.node.getSocketPosition(self.index, self.position)
+        if DEBUG: print("  res", res)
+        return res
+
+    def setConnectedEdge(self, edge=None):
+        self.edge = edge
