9e7522d49c8edfac0cbf407bd5d2e488c193a5e1
luoruchong <luoruchong@bytedance.com>
2022-02-01 21:30:24 +0800

    learning-17: 实现load功能

--- node_socket.py
+++ node_socket.py
@@ -50,5 +50,7 @@ class Socket(Serializable):
         ])
 
     def deserialize(self, data, hashmap={}):
-        return False
+        self.id = data['id']
+        hashmap[data['id']] = self
+        return True
 
